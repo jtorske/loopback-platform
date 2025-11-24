b829ff6a011c9b582374143aff1ee0cd5abc3a03
ri-yad-a <riyad.abdullayev@ucalgary.ca>
2025-11-24 08:08:00 -0700

    Initial Login and Register structure complete

--- frontend/loopback/src/app/app.routes.ts
+++ frontend/loopback/src/app/app.routes.ts
@@ -4,12 +4,16 @@ import { Landing } from './pages/landing/landing';
 import { Products } from './pages/products/products';
 import { Companies } from './pages/companies/companies';
 import { Account } from './pages/account/account';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
   { path: '', component: Landing },
   { path: 'products', component: Products },
   { path: 'companies', component: Companies },
   { path: 'account', component: Account },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
 
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
