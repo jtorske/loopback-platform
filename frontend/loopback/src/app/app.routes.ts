e400929fb502262debd88b75ac0c042af72898ea
g-ashar <gaurav.ashar@ucalgary.ca>
2025-11-24 21:06:29 -0700

    Add route to company dashboard page

--- frontend/loopback/src/app/app.routes.ts
+++ frontend/loopback/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { Landing } from './pages/landing/landing';
 import { Products } from './pages/products/products';
 import { Companies } from './pages/companies/companies';
 import { Account } from './pages/account/account';
+import { CompanyDashboard } from './pages/company-dashboard/company-dashboard';
 
 export const routes: Routes = [
   { path: '', component: Landing },
   { path: 'products', component: Products },
   { path: 'companies', component: Companies },
+  { path: 'company-dashboard', component: CompanyDashboard },
   { path: 'account', component: Account },
 
   { path: '**', redirectTo: '', pathMatch: 'full' },
